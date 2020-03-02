878cb2d060c60654769e5d3fd33ef1aef3fa99d4
Morris Nathan A <NMorris@uwhealth.org>
2020-03-02 12:47:18 -0600

    Enhancing create_snapshot with a timer, memory and quiesce options

--- samples/create_snapshot.py
+++ samples/create_snapshot.py
@@ -18,12 +18,15 @@ import atexit
 
 import requests
 
+import time
+
 from pyVim.connect import SmartConnect, SmartConnectNoSSL, Disconnect
 
 from tools import cli
 
 requests.packages.urllib3.disable_warnings()
 
+SNAPSHOT_LIMIT_SECONDS = 180
 
 def setup_args():
     parser = cli.build_arg_parser()
@@ -37,6 +40,12 @@ def setup_args():
                         help="Description for the snapshot")
     parser.add_argument('-n', '--name', required=True,
                         help="Name for the Snapshot")
+    parser.add_argument('-q', '--quiesce', required=False,
+                        action='store_true',
+                        help="Quiesce enabled for snapshot")
+    parser.add_argument('-m', '--memory', required=False,
+                        action='store_true',
+                        help="Memory enabled for snapshot")
     my_args = parser.parse_args()
     return cli.prompt_for_password(my_args)
 
@@ -73,18 +82,41 @@ desc = None
 if args.description:
     desc = args.description
 
+# memory and quiesce are mutually exclusive
+quiesce_flag = False
+memory_flag = False
+if args.quiesce:
+    quiesce_flag = True
+else:
+    if args.memory:
+        memory_flag = True
+        quiesce_flag = False
+
 task = vm.CreateSnapshot_Task(name=args.name,
+                              memory=memory_flag,
                               description=desc,
-                              memory=True,
-                              quiesce=False)
-print("Snapshot Completed.")
+                              quiesce=quiesce_flag)
+
+counter = 0
+while task.info.state not in ['success','error'] and counter < SNAPSHOT_LIMIT_SECONDS:
+    time.sleep(1)
+    counter += 1
+print("Snapshot final status after {0} seconds: {1}".format(counter, task.info.state))
+
 del vm
 vm = si.content.searchIndex.FindByUuid(None, args.uuid, True, instance_search)
+# Enable a short delay or 3 seconds so the tree should include the current snapshot for fast snapshots
+if counter < 10:
+    time.sleep(3)
 snap_info = vm.snapshot
 
-tree = snap_info.rootSnapshotList
-while tree[0].childSnapshotList is not None:
-    print("Snap: {0} => {1}".format(tree[0].name, tree[0].description))
-    if len(tree[0].childSnapshotList) < 1:
-        break
-    tree = tree[0].childSnapshotList
+if snap_info is not None:
+    print("Existing snapshots:")
+    tree = snap_info.rootSnapshotList
+    while tree[0].childSnapshotList is not None:
+        print("Snap: {0} => {1}".format(tree[0].name, tree[0].description))
+        if len(tree[0].childSnapshotList) < 1:
+            break
+        tree = tree[0].childSnapshotList
+else:
+    print("No other snapshots currently exist. There was an error or the the snapshot happened too quickly to register.")
